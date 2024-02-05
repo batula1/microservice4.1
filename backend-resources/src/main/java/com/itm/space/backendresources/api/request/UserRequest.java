27ef3534ea249971921a306e674ab807bc7e6782
Albert Batula <batula227@gmail.com>
2024-02-05 11:39:42 +0300

    finish

--- backend-resources/src/main/java/com/itm/space/backendresources/api/request/UserRequest.java
+++ backend-resources/src/main/java/com/itm/space/backendresources/api/request/UserRequest.java
@@ -3,16 +3,19 @@ package com.itm.space.backendresources.api.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class UserRequest {
     @NotBlank(message = "Username should not be blank")
     @Size(min = 2, max = 30, message = "Username should be between 2 and 30 characters long")
     private final String username;
     @NotBlank(message = "Email should not be blank")
     @Email(message = "Email should be valid", regexp = ".+@.+\\..+")
-
     private final String email;
     @NotBlank(message = "Password should not be blank")
     @Size(min = 4, message = "Password should be greater than 4 characters long")
